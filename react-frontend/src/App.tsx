72f805a0394c90760599b797c95d05b923f0d6f2
willsonr8 <willsonr8@gmail.com>
2024-03-24 09:22:03 -0400

    Added to login page features.

--- react-frontend/src/App.tsx
+++ react-frontend/src/App.tsx
@@ -5,19 +5,23 @@ import QueryOpenings from './query_openings';
 import QueryResults from './query_results';
 import Navigation from './navigation';
 import Home from './home';
+import UserLogin from './user_login'
 
 
 function App() {
 
+  const [loggedIn, setLoggedIn] = useState(false)
+  const [user, setUser] = useState('')
   return (
     <div>
       <nav>
         <Navigation />
       </nav>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home user={user} loggedIn={loggedIn} setLoggedIn={setLoggedIn} />} />
         <Route path="/query-openings" element={<QueryOpenings />} />
         <Route path="/query-results" element={<QueryResults />} />
+        <Route path="/user-login" element={<UserLogin setLoggedIn={setLoggedIn} setUser={setUser} />} />
       </Routes>
 
       

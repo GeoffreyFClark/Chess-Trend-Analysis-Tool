427679c85ef5844d3a9134bea588d20bcabb3d0d
willsonr8 <willsonr8@gmail.com>
2024-04-22 22:59:53 -0400

    sync main repo with willsonr8/rep0. stash and recommit query 3 add-ins.

--- python-backend/queryhelper.py
+++ python-backend/queryhelper.py
@@ -6,7 +6,7 @@ def where_eco_code(eco_code):
     return where_clause
 
 
-# calculates win rates of all ecocodes.
+# used in query 2: calculates win rates of all ecocodes.
 def WinRates(minGames=1, fetchRows=130):
     query = (f"SELECT ECOCODE, "
                 f"SUM(CASE WHEN RESULT = '1-0' THEN 1 ELSE 0 END) AS Wins, "
@@ -20,7 +20,7 @@ def WinRates(minGames=1, fetchRows=130):
     return query
 
 
-# calculates average turns in loss of all ecocodes. Can yield average turns in loss for just one eco code as well.
+# used in query 2: calculates average turns in loss of all ecocodes. Can yield average turns in loss for just one eco code as well.
 def AvgMovesInLoss(eco_code=None, fetchRows=130):
     where_clause = where_eco_code(eco_code)
     query = (f"SELECT ECOCODE, "
@@ -32,6 +32,7 @@ def AvgMovesInLoss(eco_code=None, fetchRows=130):
              f"FETCH FIRST {fetchRows} ROWS ONLY")
     return query
 
+# used in query 3
 def TotalGamesInMonthYear():
     query = (f"SELECT COUNT(*) AS Games, "
              f"EXTRACT(MONTH FROM EVENTDATE) AS Month, "
@@ -40,6 +41,34 @@ def TotalGamesInMonthYear():
              f"GROUP BY EXTRACT(MONTH FROM EVENTDATE), EXTRACT(YEAR FROM EVENTDATE)")
     return query
 
+# used in query 3
+def UserSelectedGames(low_white_elo=246, high_white_elo=3958, low_black_elo=246, high_black_elo=3958, low_turn=1, high_turn=201, start_date="01-JAN-1942", end_date = "01-JAN-2024"):
+    query = (f"SELECT * FROM GAMES2 "
+             f"WHERE WHITEELO >= {low_white_elo} AND WHITEELO <= {high_white_elo} "
+             f"AND BLACKELO >= {low_black_elo} AND BLACKELO <= {high_black_elo} "
+             f"AND TURNS >= {low_turn} AND TURNS <= {high_turn} "
+             f"AND EVENTDATE >= TO_DATE('{start_date}', 'DD-MON-YYYY') "
+             f"AND EVENTDATE <= TO_DATE('{end_date}', 'DD-MON-YYYY')")
+    return query
+
+# used in query 3
+def DifferenceData():
+    query = (f"SELECT ABS(WHITEELO - BLACKELO) AS Difference, "
+             f"EXTRACT(YEAR FROM EVENTDATE) AS Year, "
+             f"(SUM(CASE WHEN (RESULT = '0-1' AND WHITEELO < BLACKELO) OR (RESULT = '1-0' AND WHITEELO > BLACKELO) THEN 1 ELSE 0 END) / COUNT(*)) AS SampleProbability, "
+             f"(1 / (1 + POWER(10, -(ABS(WHITEELO - BLACKELO) / 400)))) AS ExpectedProbability, COUNT(*) AS Occurrences "
+             f"FROM UserSelectedGames "
+             f"GROUP BY ABS(WHITEELO - BLACKELO), EXTRACT(YEAR FROM EVENTDATE)")
+    return query
+
+# used in query 3
+def YearTotals():
+    query = (f"SELECT Year, "
+             f"SUM(SampleProbability * Occurrences) / SUM(Occurrences) AS SampleYearProbability, "
+             f"SUM(ExpectedProbability * Occurrences) / SUM(Occurrences) AS ExpectedYearProbability, "
+             f"SUM(Occurrences) AS OccurrencesPerYear "
+             f"FROM DifferenceData GROUP BY Year")
+    return query
 
 # query 2: Runs query 2 fully and allows various inputs
 def query2(min_Games=1, start_date="JAN-2018", end_date="DEC-2023", fetch_Rows=130):
@@ -55,9 +84,21 @@ def query2(min_Games=1, start_date="JAN-2018", end_date="DEC-2023", fetch_Rows=1
             f"JOIN GamesInMonthYear ON EXTRACT(MONTH FROM GAMES2.EVENTDATE) = GamesInMonthYear.Month "
                 f"AND EXTRACT(YEAR FROM GAMES2.EVENTDATE) = GamesInMonthYear.Year "
             f"WHERE GAMES2.EVENTDATE <= TO_DATE('{end_date}', 'MON-YYYY') "
-                f"AND GAMES2.EVENTDATE >= TO_DATE('{start_date}', 'MON-YYYY')"
+                f"AND GAMES2.EVENTDATE >= TO_DATE('{start_date}', 'MON-YYYY') "
             f"GROUP BY EXTRACT(MONTH FROM GAMES2.EVENTDATE), EXTRACT(YEAR FROM GAMES2.EVENTDATE), GamesInMonthYear.Games "
             f"ORDER BY Year, Month")
+    print(query)
+    return query
+
+# query 3
+def query3(low_white_elo=246, high_white_elo=3958, low_black_elo=246, high_black_elo=3958, low_turn=1, high_turn=201, start_date="01-JAN-1942", end_date = "01-JAN-2024"):
+    query = (f"WITH UserSelectedGames AS ({UserSelectedGames(low_white_elo=low_white_elo, high_white_elo=high_white_elo, low_black_elo=low_black_elo, high_black_elo=high_black_elo, low_turn=low_turn, high_turn=high_turn, start_date=start_date, end_date=end_date)}), "
+             f"DifferenceData AS ({DifferenceData()}), "
+             f"YearTotals AS ({YearTotals()}) "
+             f"SELECT Year, ROUND((SampleYearProbability / ExpectedYearProbability), 3) AS SampleOverExpected "
+             f"FROM YearTotals "
+             f"WHERE OccurrencesPerYear >= 250 "
+             f"ORDER BY Year DESC")
     print(query)
     return query
 
--- python-backend/server.py
+++ python-backend/server.py
@@ -52,7 +52,7 @@ def sql_complex_trend_query_2():
     return queryhelper.query2()
 
 def sql_complex_trend_query_3():
-    return "SELECT * FROM games2 FETCH FIRST 6 ROWS ONLY"
+    return queryhelper.query3()
 
 def sql_complex_trend_query_4():
     return "SELECT * FROM games2 FETCH FIRST 8 ROWS ONLY"
